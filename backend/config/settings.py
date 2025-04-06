17eca7e7bbc3f9e3f33147bc5584e4c51b8d65f2
Eazy-Snipz47 <chimanwaeme@gmail.com>
2025-04-05 20:01:11 -0400

    Database update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3', #Configure back to sqlite3
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'your_username',   #   Retieve username
+        'PASSWORD': 'your_password',  #Retrieve password
+        'HOST': 'localhost',
+        'PORT':'5432',
     }
 }
 

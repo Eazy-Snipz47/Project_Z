f45016331e456417c41d55aaac26b33e8ac2379a
LeslieKuepouo <lchoulewomkuepouo@widener.edu>
2025-05-26 17:34:05 -0400

    saved models in admin.py

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import(
+    RideRequest, 
+    DriverProfile, 
+    Vehicle, 
+    Location, 
+    UserProfile,
+    Ride,
+    Notification,
+    SupportTicket,
+    SavedLocation,
+    DriverAvailability
+)
+
+admin.site.register(RideRequest)
+admin.site.register(DriverProfile)
+admin.site.register(Vehicle)
+admin.site.register(Location)
+admin.site.register(UserProfile)
+admin.site.register(Ride)
+admin.site.register(Notification)
+admin.site.register(SupportTicket)
+admin.site.register(SavedLocation)
+admin.site.register(DriverAvailability)
 
 # Register your models here.
